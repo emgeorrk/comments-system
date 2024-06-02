7e3795f1169c3d9332481ab7cb5e8c50e8f27d08
Egor <nounstep@gmail.com>
2024-06-02 23:14:32 +0300

    Feature: resolvers moved to resolvers.go

---
+++ graphql/resolvers.go
@@ -0,0 +1,86 @@
+package gql
+
+import (
+	"errors"
+	"fmt"
+	"github.com/graphql-go/graphql"
+	"graphql-comments/storage"
+)
+
+func addPostResolver(params graphql.ResolveParams) (interface{}, error) {
+	title, _ := params.Args["title"].(string)
+	content, _ := params.Args["content"].(string)
+	allowComments, ok := params.Args["allowComments"].(bool)
+
+	switch {
+	case title == "":
+		return nil, errors.New("title is empty")
+	case content == "":
+		return nil, errors.New("content is empty")
+	}
+
+	if !ok {
+		allowComments = true
+	}
+	newPost, err := storage.DataBase.AddPost(title, content, allowComments)
+	if err != nil {
+		return nil, err
+	}
+	return newPost, nil
+}
+
+func addCommentResolver(params graphql.ResolveParams) (interface{}, error) {
+	postID, _ := params.Args["postId"].(string)
+	parentCommentID, _ := params.Args["parentCommentId"].(string)
+	content, _ := params.Args["content"].(string)
+
+	switch {
+	case postID == "":
+		return nil, errors.New("postID is empty")
+	case content == "":
+		return nil, errors.New("content is empty")
+	case len(content) > storage.MaxCommentLength:
+		return nil, errors.New(fmt.Sprintf("content is too long (maximum %d chars)", storage.MaxCommentLength))
+	}
+
+	newComment, err := storage.DataBase.AddComment(postID, parentCommentID, content)
+	if err != nil {
+		return nil, err
+	}
+	return newComment, nil
+}
+
+func getPostsResolver(params graphql.ResolveParams) (interface{}, error) {
+	posts, err := storage.DataBase.GetPosts()
+	if err != nil {
+		return nil, err
+	}
+	return posts, nil
+}
+
+func getPostByIDResolver(params graphql.ResolveParams) (interface{}, error) {
+	id, _ := params.Args["id"].(string)
+	post, err := storage.DataBase.GetPostByID(id)
+	if err != nil {
+		return nil, err
+	}
+	return post, nil
+}
+
+func getCommentsResolver(params graphql.ResolveParams) (interface{}, error) {
+	postId, _ := params.Args["postId"].(string)
+	comments, err := storage.DataBase.GetComments(postId)
+	if err != nil {
+		return nil, err
+	}
+	return comments, nil
+}
+
+func getCommentByIDResolver(params graphql.ResolveParams) (interface{}, error) {
+	id, _ := params.Args["id"].(string)
+	comment, err := storage.DataBase.GetCommentByID(id)
+	if err != nil {
+		return nil, err
+	}
+	return comment, nil
+}
---
+++ graphql/schema.go
@@ -0,0 +1,131 @@
+package gql
+
+import (
+	"github.com/graphql-go/graphql"
+)
+
+// PostType определяет тип постов для GraphQL
+var PostType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Post",
+	Fields: graphql.Fields{
+		"id": &graphql.Field{
+			Type: graphql.NewNonNull(graphql.ID),
+		},
+		"title": &graphql.Field{
+			Type: graphql.NewNonNull(graphql.String),
+		},
+		"content": &graphql.Field{
+			Type: graphql.NewNonNull(graphql.String),
+		},
+		"createdAt": &graphql.Field{
+			Type: graphql.NewNonNull(graphql.String),
+		},
+		"comments": &graphql.Field{
+			Type: graphql.NewNonNull(graphql.NewList(graphql.ID)),
+		},
+		"allowComments": &graphql.Field{
+			Type: graphql.NewNonNull(graphql.Boolean),
+		},
+	},
+})
+
+// CommentType определяет тип комментариев для GraphQL
+var CommentType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Comment",
+	Fields: graphql.Fields{
+		"id": &graphql.Field{
+			Type: graphql.NewNonNull(graphql.ID),
+		},
+		"postId": &graphql.Field{
+			Type: graphql.NewNonNull(graphql.ID),
+		},
+		"parentCommentId": &graphql.Field{
+			Type: graphql.ID,
+		},
+		"content": &graphql.Field{
+			Type: graphql.NewNonNull(graphql.String),
+		},
+		"createdAt": &graphql.Field{
+			Type: graphql.NewNonNull(graphql.String),
+		},
+		"replies": &graphql.Field{
+			Type: graphql.NewNonNull(graphql.NewList(graphql.ID)),
+		},
+	},
+})
+
+// QueryType определяет типы запросов для GraphQL
+var QueryType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Query",
+	Fields: graphql.Fields{
+		"getPosts": &graphql.Field{
+			Name:    "getPosts",
+			Type:    graphql.NewList(PostType),
+			Resolve: getPostsResolver,
+		},
+		"getPostByID": &graphql.Field{
+			Type: PostType,
+			Args: graphql.FieldConfigArgument{
+				"id": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.ID),
+				},
+			},
+			Resolve: getPostByIDResolver,
+		},
+		"getComments": &graphql.Field{
+			Type: graphql.NewList(CommentType),
+			Args: graphql.FieldConfigArgument{
+				"postId": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.ID),
+				},
+				"paginationSize": &graphql.ArgumentConfig{
+					Type: graphql.Int,
+				},
+			},
+			Resolve: getCommentsResolver,
+		},
+		"getCommentByID": &graphql.Field{
+			Type: PostType,
+			Args: graphql.FieldConfigArgument{
+				"id": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.ID),
+				},
+			},
+			Resolve: getCommentByIDResolver,
+		},
+	},
+})
+
+// MutationType определяет мутации для GraphQL
+var MutationType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Mutation",
+	Fields: graphql.Fields{
+		"addPost": &graphql.Field{
+			Type: PostType,
+			Args: graphql.FieldConfigArgument{
+				"title": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.String),
+				},
+				"content": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.String),
+				},
+			},
+			Resolve: addPostResolver,
+		},
+		"addComment": &graphql.Field{
+			Type: CommentType,
+			Args: graphql.FieldConfigArgument{
+				"postId": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.ID),
+				},
+				"parentCommentId": &graphql.ArgumentConfig{
+					Type: graphql.ID,
+				},
+				"content": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.String),
+				},
+			},
+			Resolve: addCommentResolver,
+		},
+	},
+})
